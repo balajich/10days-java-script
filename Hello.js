ff52caf6fd9d4789d42812f3f7bde5790e6573da
Balaji Chopparapu <balaji.chopparapu@gmail.com>
2018-08-16 20:50:35 +0530

    updated

--- Hello.js
+++ Hello.js
@@ -1,2 +1,15 @@
 console.log('Hello World')
 console.log('I am liking this')
+
+
+function greeting(message) {
+    console.log(message);
+}
+
+greeting('This is the way you call functions')
+
+var x = 123;
+function greeting2() {
+    console.log(x)
+}
+greeting2()
